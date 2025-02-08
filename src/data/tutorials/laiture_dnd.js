44aa852f6979a29fecc3907c859460d64e73dd08
Fish <fishagbalo@gmail.com>
2025-02-08 22:11:23 +0800

    feat: enhance drag-and-drop functionality by allowing item transfers between stores in Laiture_dnd component

--- src/data/tutorials/laiture_dnd.js
+++ src/data/tutorials/laiture_dnd.js
@@ -22,6 +22,35 @@ function Laiture_dnd() {
       const [removedStore] = reorderedStores.splice(sourceIndex, 1);
       reorderedStores.splice(destinationIndex, 0, removedStore);
       setStores(reorderedStores);
+    } else {
+      const storeSourceIndex = stores.findIndex(
+        (store) => store.id === source.droppableId
+      );
+
+      const storeDestinationIndex = stores.findIndex(
+        (store) => store.id === destination.droppableId
+      );
+
+      const newSourceItems = [...stores[storeSourceIndex].items];
+      const newDestinationItems =
+        source.droppableId !== destination.droppableId
+          ? [...stores[storeDestinationIndex].items]
+          : newSourceItems;
+      const [deletedItem] = newSourceItems.splice(source.index, 1);
+      newDestinationItems.splice(destination.index, 0, deletedItem);
+
+      const newStores = [...stores];
+      newStores[storeSourceIndex] = {
+        ...stores[storeSourceIndex],
+        items: newSourceItems,
+      };
+      if (source.droppableId !== destination.droppableId) {
+        newStores[storeDestinationIndex] = {
+          ...stores[storeDestinationIndex],
+          items: newDestinationItems,
+        };
+      }
+      setStores(newStores);
     }
   };
 
@@ -46,9 +75,8 @@ function Laiture_dnd() {
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
-                        className="store-container"
                       >
-                        <h3>{store.name}</h3>
+                        <StoreList {...store} />
                       </div>
                     )}
                   </Draggable>
@@ -60,6 +88,41 @@ function Laiture_dnd() {
         </DragDropContext>
       </div>
     </div>
+  );
+}
+
+function StoreList({ name, items = [], id }) {
+  return (
+    <Droppable droppableId={id} type="item">
+      {(provided) => (
+        <div
+          className="store"
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+        >
+          <div className="store-container">
+            <h3>{name}</h3>
+          </div>
+          <div className="item-container">
+            {items.map((item, index) => (
+              <Draggable draggableId={item.id} index={index} key={item.id}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    <h4>{item.name}</h4>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        </div>
+      )}
+    </Droppable>
   );
 }
 

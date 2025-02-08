905d7c900f80a92e987f1d30ad30a3e9c9084547
Fish <fishagbalo@gmail.com>
2025-02-08 21:21:30 +0800

    refactor: simplify App component and remove unused files; add initial data and tutorial components

---
+++ src/data/tutorials/laiture_dnd.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./laiture_dnd.css";
+function laiture_dnd() {
+  return (
+    <div className="layout__wrapper">
+      <div class="card">
+        <div class="header">
+          <h1>Shopping List</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default laiture_dnd;
